ccf9a6e060bbe55f7f55ebd5b13681aa3d2c5f11
6185jcaron <66760646+Ace-03@users.noreply.github.com>
2024-09-17 11:24:40 -0700

    created web build

--- Hydra_Havoc/Assets/Scripts/CheckersBaordScript.cs
+++ Hydra_Havoc/Assets/Scripts/CheckersBaordScript.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
-using UnityEditor.XR;
+//using UnityEditor.XR;
 using UnityEngine;
 
 public class CheckersBaordScript : MonoBehaviour
